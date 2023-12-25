da47a857d7179fa0f2ee6bfafc08a1fb0487cd88
Estefani Lira <estefaniwanderley.lira@gmail.com>
2023-12-25 19:12:21 -0300

    DESAFIO CONCLUIDO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Celular Nokia:");
+Smartphone nokia = new Nokia(
+    numero: "987654321",
+    modelo: "modelo 1",
+    imei: "12345",
+    memoria: 128
+);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Celular Iphone");
+Smartphone iphone = new Iphone(
+    numero: "123456789",
+    modelo: "iphone 14",
+    imei: "298754",
+    memoria: 256
+);
+iphone.Ligar();
+iphone.InstalarAplicativo("WhatsApp");
+Console.WriteLine("\nPrograma concluído.");
